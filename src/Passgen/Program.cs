2b89d07586877d0d2879e62993b31da9b9a3b398
Arvydas Grigonis <arnvidhr@users.noreply.github.com>
2024-05-13 20:49:47 +0300

    Creation

---
+++ src/Passgen/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Passgen
+{
+    internal class Program
+    {
+        #region Const
+        const char C_TITLE_FILLER = '*';
+        readonly static string STR_P = new string(C_TITLE_FILLER, 2);
+        const string STR_APP_NAME = "Passgen";
+
+        #endregion
+
+        #region Properties
+        static string CopyrightYear
+        {
+            get
+            {
+                var retVal = "2024";
+
+                if (DateTime.Now.Year < 2024)
+                    retVal += "-" + DateTime.Now.Year;
+
+                return retVal;
+            }
+        }
+
+        #endregion
+
+        #region Entry
+        static void Main(string[] args)
+        {
+            AppStart();
+
+            var gen = new RandomPasswordGenerator(args);
+            if(gen.Validate())
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Generated passwords below:");
+                Console.WriteLine();
+
+                var list = gen.Generate();
+
+                foreach (var item in list)
+                {
+                    Console.WriteLine($"{item}\r\n");
+                }
+            }
+            else
+            {
+                AppUsage();
+            }
+
+            AppEnd();
+        }
+
+        #endregion
+
+        #region Helpers
+
+        static void AppStart()
+        {
+            Console.WriteLine(new string(C_TITLE_FILLER, 80));
+            Console.WriteLine($"{STR_P} Passgen (C) {CopyrightYear} Arvydas Grigonis");
+            Console.WriteLine(new string(C_TITLE_FILLER, 80));
+        }
+        
+        static void AppEnd()
+        {
+            Console.WriteLine("Read Any Key To Continue..");
+            Console.ReadKey();
+        }
+
+        static void AppUsage()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{STR_P} Usage: {STR_APP_NAME} [number of passwords] [length] [used symbols]");
+            Console.WriteLine();
+            Console.WriteLine($"{STR_P} Where used sybols can be:");
+            Console.WriteLine($"{STR_P} \td - digits");
+            Console.WriteLine($"{STR_P} \tu - upper case letters");
+            Console.WriteLine($"{STR_P} \tl - lower case letters");
+            Console.WriteLine($"{STR_P} \ts - symbols");
+            Console.WriteLine();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Passgen/RandomPasswordGenerator.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Passgen
+{
+    /// <summary>
+    /// Random passwords generator class (C) 2024 Arvydas Grigonis
+    /// </summary>
+    internal class RandomPasswordGenerator
+    {
+        #region Attrs
+        readonly Random Random = new();
+
+        string[] _Args;
+
+        bool _Validated = false;
+
+        int _NumberOfPasswords = 0;
+        int _PasswordLength = 0;
+
+        bool _UseDigits = false;
+        bool _UseUppercaseCharacters = false;
+        bool _UseLowercaseCharacters = false;
+        bool _UseSymbols = false;
+
+        string _AvailableLetters = string.Empty;
+
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="args"></param>
+        internal RandomPasswordGenerator(string[] args)
+        {
+            _Args = args;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        /// <summary>
+        /// Initialization before generation
+        /// </summary>
+        void Initialize()
+        {
+            if(string.IsNullOrEmpty(_AvailableLetters))
+            {
+                if(_UseDigits)
+                    _AvailableLetters += GenerateString('0', '9');
+
+                if (_UseUppercaseCharacters)
+                    _AvailableLetters += GenerateString('A', 'Z');
+
+                if (_UseLowercaseCharacters)
+                    _AvailableLetters += GenerateString('a', 'z');
+
+                if(_UseSymbols)
+                {
+                    _AvailableLetters += GenerateString('!', '/');
+                    _AvailableLetters += GenerateString(':', '`');
+                }
+            }
+        }
+
+        string GenerateString(char from, char to)
+        {
+            var retVal = string.Empty;
+
+            for (char i = from; i < (to + 1); i++)
+            {
+                retVal += i;
+            }
+            return retVal;
+        }
+
+        /// <summary>
+        /// Single password generation
+        /// </summary>
+        /// <returns></returns>
+        string GeneratePassword()
+        {
+            var len = _PasswordLength;
+            var chars = new char[len];
+
+            while (--len >= 0)
+                chars[len] = _AvailableLetters[Random.Next(_AvailableLetters.Length)];
+
+            return new string(chars);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Validation and parsing of params
+        /// </summary>
+        /// <returns></returns>
+        internal bool Validate()
+        {
+            // Args
+            if (_Args.Length < 3)
+                return false;
+
+            // Number of passwords
+            if (!int.TryParse(_Args[0], out _NumberOfPasswords))
+                return false;
+
+            if (_NumberOfPasswords < 1)
+                return false;
+
+            // Password length
+            if (!int.TryParse(_Args[1], out _PasswordLength))
+                return false;
+
+            if(_PasswordLength < 1)
+                return false;
+
+            // duls!
+            _UseDigits = _Args[2].Contains('d', StringComparison.OrdinalIgnoreCase);
+            _UseUppercaseCharacters = _Args[2].Contains('u', StringComparison.OrdinalIgnoreCase);
+            _UseLowercaseCharacters = _Args[2].Contains('l', StringComparison.OrdinalIgnoreCase);
+            _UseSymbols = _Args[2].Contains('s', StringComparison.OrdinalIgnoreCase);
+
+            Initialize();
+
+            // Generation valid if we have valid
+            _Validated = _AvailableLetters.Length > 0 && 
+                (_UseDigits || _UseUppercaseCharacters || _UseLowercaseCharacters || _UseSymbols);
+
+            return _Validated;
+        }
+
+        /// <summary>
+        /// Generate entry point
+        /// </summary>
+        /// <returns></returns>
+        internal List<string> Generate()
+        {
+            var retVal = new List<string>();
+
+            if(_Validated)
+            {
+                for (int i = 0; i < _NumberOfPasswords; i++)
+                {
+                    retVal.Add(GeneratePassword());
+                }
+            }
+            return retVal;
+        }
+
+        #endregion
+    }
+}
